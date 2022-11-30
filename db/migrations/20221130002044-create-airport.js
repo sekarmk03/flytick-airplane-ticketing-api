76836b4294163250ad8fde6c0b551b629eafa15b
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2022-11-30 07:39:19 +0700

    Add seeders (country, city, airport, flight), models (airport, city)

---
+++ db/migrations/20221130002044-create-airport.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      code: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      city_id: {
+        type: Sequelize.INTEGER
+      },
+      country_id: {
+        type: Sequelize.INTEGER
+      },
+      maps_link: {
+        type: Sequelize.STRING
+      },
+      maps_embed: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airports');
+  }
+};
