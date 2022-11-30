70b2588e6d30926bb9267790b648a577e3453d49
Charles <charlesgunawan32@gmail.com>
2022-11-30 11:29:34 +0700

    add notification model, migration, controller, and route

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const oauth = require('./oauth');
 const user = require('./user');
 const flight = require('./flight');
 const country = require('./country');
+const notif = require('./notification')
 
 router.use('/auth', auth);
 router.use('/oauth', oauth);
 router.use('/users', user);
 router.use('/flight', flight);
 router.use('/country', country);
+router.use('/notification', notif);
 
 module.exports = router;
