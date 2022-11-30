02cfe0f5833d65ce20afa0d474e2ece37a4f7342
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2022-11-30 08:43:51 +0700

    Add authorization role for country and flight routes

--- routes/flight.js
+++ routes/flight.js
@@ -9,10 +9,10 @@ router.get('/', flight_c.getAll);
 // get detail flight
 router.get('/:id', flight_c.getDetail);
 // create flight
-router.post('/create', /*authorize([roles.admin, roles.superadmin]),*/ flight_c.create);
+router.post('/create', authorize([roles.admin, roles.superadmin]), flight_c.create);
 // update flight
-router.put('/:id', /*authorize([roles.admin, roles.superadmin]),*/ flight_c.update);
+router.put('/:id', authorize([roles.admin, roles.superadmin]), flight_c.update);
 // delete flight
-router.delete('/:id', /*authorize([roles.admin, roles.superadmin]),*/ flight_c.delete);
+router.delete('/:id', authorize([roles.admin, roles.superadmin]), flight_c.delete);
 
 module.exports = router;
