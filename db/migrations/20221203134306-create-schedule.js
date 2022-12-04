5fe85756464a5f58c36dde3b20b8827e21970bfe
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2022-12-04 21:42:56 +0700

    Update passenger

--- db/migrations/20221203134306-create-schedule.js
+++ db/migrations/20221203134306-create-schedule.js
@@ -26,6 +26,9 @@ module.exports = {
       to_airport: {
         type: Sequelize.INTEGER
       },
+      passenger: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
