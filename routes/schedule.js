1342942c7474c47286d04539f8d1ea8c43433174
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2022-12-04 14:25:22 +0700

    Feat: generate qr ticket, update check in status

---
+++ routes/schedule.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const c = require('../controllers/schedule');
+const authorize = require('../middleware/authorize');
+const roles = require('../utils/roles');
+
+// get all ticket
+router.get('/', authorize([roles.admin, roles.superadmin]), c.index);
+// get detail ticket
+router.get('/:ticketId', authorize([roles.admin, roles.superadmin]), c.show);
+// create ticket
+router.post('/', authorize([roles.admin, roles.superadmin]), c.create);
+// update ticket
+router.put('/:ticketId', authorize([roles.admin, roles.superadmin]), c.update);
+// scan ticket
+// router.put('/:ticketId', authorize([roles.admin, roles.superadmin]), c.update);
+router.put('/:ticketId', c.update_checked_in);
+// delete ticket
+router.delete('/:ticketId', authorize([roles.admin, roles.superadmin]), c.delete);
+
+module.exports = router;
---
+++ routes/transaction.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const c = require('../controllers/transaction');
+const authorize = require('../middleware/authorize');
+const roles = require('../utils/roles');
+
+// get all transaction
+router.get('/', authorize([roles.admin, roles.superadmin]), c.index);
+// get detail user
+router.get('/:transactionId', authorize([roles.admin, roles.superadmin]), c.show);
+// create user
+router.post('/', authorize([roles.admin, roles.superadmin]), c.create);
+
+module.exports = router;
