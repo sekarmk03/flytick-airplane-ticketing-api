ab80429338d5b79d5e2d89c050ef3599bfa09912
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2022-12-07 19:32:38 +0700

    Add transaction and schedule seeders

---
+++ db/seeders/20221205045410-Transaction.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Transactions', [
+    {
+      transaction_time: new Date(),
+      invoice_number: 'DUMMY-010101',
+      user_id: 1,
+      paid_time: new Date(),
+      paid_status: true,
+      adult: 2,
+      child: 1,
+      round_trip: true,
+      total_cost: 15400000,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      transaction_time: new Date(),
+      invoice_number: 'DUMMY-020202',
+      user_id: 2,
+      paid_time: new Date(),
+      paid_status: true,
+      adult: 2,
+      child: 0,
+      round_trip: false,
+      total_cost: 12000000,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+   ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Transactions', null, {});
+  }
+};
